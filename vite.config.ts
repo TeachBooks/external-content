15e41d8d58ddd521106221e4c74700c7acc86a0b
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-03-06 11:01:01 +0100

    Add Solid-UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import tsconfigPaths  from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [solidPlugin(), tailwindcss()],
+  plugins: [ tsconfigPaths(), solidPlugin(), tailwindcss()],
   server: {
     port: 3000,
   },
