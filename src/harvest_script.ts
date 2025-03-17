9fb0098bb0929a18f3e61898fbd64bfedd0ad274
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-03-17 16:40:05 +0100

    Generate chapters.json with js

--- src/harvest_script.ts
+++ src/harvest_script.ts
@@ -1,16 +1,17 @@
-import { parse } from 'yaml'
-import { readFile, writeFile } from 'node:fs/promises'
-import type { Book } from './store.ts';
-import { harvestBook, type BookQuery } from './harvest.ts';
+import { readFile, writeFile } from "node:fs/promises";
+import { parse } from "yaml";
+import { type BookQuery, harvestBook } from "./harvest.ts";
+import type { Book } from "./store.ts";
 
-async function main(books_file='books.yml', chapters_file='chapters.json') {
-    const queries = parse(await readFile(books_file, 'utf-8')) as BookQuery[]
-    const books: Book[] = []
-    for (const query of queries) {
-        const book = await harvestBook(query)
-        books.push(book)
-    }
-    await writeFile(chapters_file, JSON.stringify(books, null, 2))
+async function main(books_file = "books.yml", chapters_file = "chapters.json") {
+  const queries = parse(await readFile(books_file, "utf-8")) as BookQuery[];
+  const books: Book[] = [];
+  for (const query of queries) {
+    console.log("Harvesting chapters for: ", query.html_url);
+    const book = await harvestBook(query);
+    books.push(book);
+  }
+  await writeFile(chapters_file, JSON.stringify(books, null, 2));
 }
 
 await main();
